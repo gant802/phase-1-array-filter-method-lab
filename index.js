2bd2b90ce4aee8ea03a1d8b017c26aeaaa2542bd
Grant Cummings <grantcummings4429@gmail.com>
2024-04-05 15:55:27 -0400

    finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(arr, str) {
+    const matchedDriver = arr.filter(driver => {
+        if (driver.toLowerCase() === str.toLowerCase()) {
+            return driver
+        }
+    })
+    return matchedDriver
+}
+
+function fuzzyMatch(arr, str) {
+    const matchedDriver = arr.filter(driver => {
+        if (driver.toLowerCase().charAt(0) === str.toLowerCase().charAt(0)) {
+            return driver
+        }
+    })
+    return matchedDriver
+}
+
+function matchName(arrOfObj, str) {
+    const matchedDriver = arrOfObj.filter(driver => {
+        if (driver.name === str)
+        return driver
+    })
+    return matchedDriver
+}
